63383b99cc59267f54f179364a68b8ff1f547724
vurucuorhan17 <vurucuorhan17@gmail.com>
2020-05-22 21:35:02 +0300

    Travis CI added

--- helpers/db.js
+++ helpers/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    mongoose.connect("mongodb://127.0.0.1:27017/node_movie_api");
+    mongoose.connect("mongodb://0.0.0.0:27017/node_movie_api");
 
     mongoose.connection.on("open",() => {
         // console.log("MongoDB: Bağlandı");
